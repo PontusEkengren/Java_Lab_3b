99b042a7b58f7da9b855e57ead5322d4115c713c
Kevin Brejcha <kebre001@gmail.com>
2013-09-27 13:40:27 +0200

    Added a manu
    If you call run() it will show the manu and take an choise

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * This class should be the class to communicate with the user
  * 
@@ -6,6 +8,7 @@
  */
 
 public class UserInterface {
+	private Scanner scan;
 	
 	public void menu(){
 		
@@ -15,6 +18,31 @@ public class UserInterface {
 		
 	}
 	public void getBooksByTitle(String title){
+		
+	}
+	public void printMenu(){
+		System.out.println("...::: Program Books:::...");
+		System.out.println("1. Add book");
+		System.out.println("2. Remove book");
+		System.out.println("3. Search for book");
+		System.out.println("4. Exit.");
+	}
+	public void run(){
+		int val = 0;
+		
+		while(val != 4){
+			printMenu();
+			val = scan.nextInt();
+			switch(val){
+			case 1: break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			}
+		}
+	}
+	
+	public static void main(String args[]){
 		
 	}
 
