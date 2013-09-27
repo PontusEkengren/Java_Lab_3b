223aa68db62a2b5a59611471d3bf003a6b6a884a
Kevin Brejcha <kebre001@gmail.com>
2013-09-27 14:07:47 +0200

    Exception & Serialize
    Added exceptions for Object Streams and Serializeable

--- src/CollectionOfBooks.java
+++ src/CollectionOfBooks.java
@@ -2,6 +2,8 @@
  * This class will do all the ordering and have the methods for finding books.
  * @author Kebre
  */
+import java.io.FileInputStream;
+import java.io.*;
 import java.util.ArrayList;
 
 public class CollectionOfBooks {
@@ -10,6 +12,36 @@ public class CollectionOfBooks {
 	
 	public void addBook(Book book){
 		
+	}
+	public void serializeToFile(String filename) throws IOException {
 		
+		ObjectOutputStream out = null;
+		
+		try {
+			out = new ObjectOutputStream(
+			new FileOutputStream(filename));
+			out.writeObject(books);
+		}
+		finally {
+			try {
+				if(out != null)	out.close();
+			} catch(Exception e) {}
+		}
+	}
+	public void deSerializeFromFile(String filename) throws IOException, ClassNotFoundException {
+		
+		ObjectInputStream in = null;
+		
+		try {
+			in = new ObjectInputStream(
+			new FileInputStream(filename));
+			// readObject returns a reference of type Object, hence the down-cast
+			books = (ArrayList<Book>) in.readObject(); 
+		}
+		finally {
+			try {
+				if(in != null)	in.close();
+			} catch(Exception e) {}			
+		}
 	}
 }
