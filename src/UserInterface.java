d16cdcfd910cfa12cfd85f337498ca3dc4d72267
Kevin Brejcha <kebre001@gmail.com>
2013-09-27 16:05:01 +0200

    Random git
    For the rest

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,20 +1,26 @@
+import java.awt.Menu;
 import java.util.Scanner;
 
 /**
  * This class should be the class to communicate with the user
  * 
- * @author Kebre
+ * @author KebreZ
  *
  */
 
 public class UserInterface {
 	private Scanner scan;
 	
-	public void menu(){
+	public UserInterface(){
+		scan = new Scanner(System.in);
+	}
+	public void addBook(){
 		
 	}
-	
-	public void addBook(){
+	public void removeBook(){
+		
+	}
+	public void searchBook(){
 		
 	}
 	public void getBooksByTitle(String title){
@@ -25,25 +31,32 @@ public class UserInterface {
 		System.out.println("1. Add book");
 		System.out.println("2. Remove book");
 		System.out.println("3. Search for book");
-		System.out.println("4. Exit.");
-	}
-	public void run(){
+		System.out.println("4. Get book by title.");
+		System.out.println("5. Exit");
+		System.out.print("Val: ");
+	}	
+	public void menu(){	
 		int val = 0;
 		
-		while(val != 4){
+		while(val != 5){
 			printMenu();
 			val = scan.nextInt();
 			switch(val){
-			case 1: break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
+			case 1: addBook(); break;
+			case 2: removeBook(); break;
+			case 3: searchBook(); break;
+			case 4: ; break;
+			case 5:  System.out.println("Bye..");break;
+			default: System.out.println("Unknown command..");
 			}
 		}
-	}
+	}	
+
+
 	
 	public static void main(String args[]){
-		
+		UserInterface menu = new UserInterface();
+		menu.menu();
 	}
 
 }
